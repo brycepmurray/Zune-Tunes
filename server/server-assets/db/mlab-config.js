f3c0947ec37a3bb7bf99c9af453b667816a9aeb2
Bryce Murray <brycepmurray711@gmail.com>
2018-03-01 15:59:37 -0700

    added models of models(modelception) images, favicon, and db config

--- server/server-assets/db/mlab-config.js
+++ server/server-assets/db/mlab-config.js
@@ -1,14 +1,14 @@
 var mongoose = require("mongoose");
 console.log('Be sure to put in your connection string!')
-var connectionString = "";
+var connectionString = 'mongodb://<dbuser>:<dbpassword>@ds048719.mlab.com:48719/musicstore';
 
 var connection = mongoose.connection;
 mongoose.connect(connectionString);
 
 connection.on("error", err => {
-  console.error("mlab Error: ", err);
+    console.error("mlab Error: ", err);
 });
 
 connection.once("open", () => {
-  console.log("connected to database");
+    console.log("connected to database");
 });
