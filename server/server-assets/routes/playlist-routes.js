4ec852c33e80631a8100821bbfe8c5f57247eeae
Bryce Murray <brycepmurray711@gmail.com>
2018-03-02 11:03:29 -0700

    playlist route done might need tweaking

---
+++ server/server-assets/routes/playlist-routes.js
@@ -0,0 +1,35 @@
+var Songs = require('../models/song')
+var Playlists = require('../models/playlist')
+var router = require('express').Router()
+
+router.post('/mytunes', (req, res, next) => {
+        Playlist.create(req.body)
+            .then(playlist => {
+                res.send(playlist)
+            })
+            .catch(err => {
+                res.status(400).send({ Error: err })
+            })
+    }),
+
+    router.delete('/mytunes/:id', (req, res, next) => {
+        Playlist.findByIdAndRemove(req.params.id)
+            .then(() => {
+                res.send({ message: 'playlist sucesfully deleted' })
+            })
+            .catch(err => {
+                res.status(400).send({ Error: err })
+            })
+    }),
+
+    router.get('/mytunes', (req, res, next) => {
+        Playlist.find({})
+            .then(playlist => {
+                res.send(playlist)
+            })
+            .catch(err => {
+                res.status(400).send({ Error: err })
+            })
+    })
+
+module.exports = router
