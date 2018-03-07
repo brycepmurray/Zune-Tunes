50e15d2e851804f450fb7d51a88c719005fca43b
Bryce Murray <brycepmurray711@gmail.com>
2018-03-06 22:40:52 -0700

    needing to figure out m-lab issue

--- server/server-assets/db/mlab-config.js
+++ server/server-assets/db/mlab-config.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 console.log('Be sure to put in your connection string!')
-var connectionString = 'mongodb://<dbuser>:<dbpassword>@ds048719.mlab.com:48719/musicstore';
+var connectionString = 'mongodb://music@admin:TestMusic@ds048719.mlab.com:48719/vue-music';
 
 var connection = mongoose.connection;
 mongoose.connect(connectionString);
