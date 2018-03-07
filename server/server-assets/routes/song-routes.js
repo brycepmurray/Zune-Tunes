f1f16b2cfcaa71cd2c2a1f67cc3af5e1bd50de8b
Bryce Murray <brycepmurray711@gmail.com>
2018-03-06 20:59:55 -0700

    add song ,  delete song, and update song in playlist by vote added

--- server/server-assets/routes/song-routes.js
+++ server/server-assets/routes/song-routes.js
@@ -1,39 +1,65 @@
 var router = require('express').Router()
 var Songs = require('..models/Song')
 
-router.post('/songs', (req, res, next) => {
+
+router.post('/mytunes/playlist', (req, res, next) => {
         Songs.create(req.body)
-            .then(post => {
-                res.send(post)
+            .then(song => {
+                return res.send(song)
             })
-            .catch(err => {
-                re.status(400).send({ Error: err })
-            })
+            .catch(next)
     }),
-    router.delete('/songs/:_id', (req, res, next) => {
-        Song.findByIdAndRemove(req.params._id)
-            .then(() => {
-                res.send({ message: 'So much for that song' })
+
+    router.delete('/mytunes/playlist/songs/:id', (req, res, next) => {
+        Songs.findByIdAndRemove(req.params.id)
+            .then(playlist => {
+                return res.send("Song Removed From Playlist")
             })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    }),
-    router.put('/songs/:_id', (req, res, next) => {
-        Song.findByIdAndUpdate(req.params._id, req.body)
-            .then(() => {
-                res.send({ message: 'Updated Position' })
-            })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    }),
-    router.get('/songs', (req, res, next) => {
-        Songs.find({})
-            .then(songs => {
-                res.send(songs)
-            })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    })
+            .catch(next)
+    });
+
+router.put('/mytunes/playlist/songs/:id', (req, res, next) => {
+    Songs.findByIdAndUpdate(req.params.id, req.body)
+        .then(song => {
+            return res.send(song)
+        })
+        .catch(next)
+})
+
+
+// router.post('/songs', (req, res, next) => {
+//         Songs.create(req.body)
+//             .then(post => {
+//                 res.send(post)
+//             })
+//             .catch(err => {
+//                 re.status(400).send({ Error: err })
+//             })
+//     }),
+//     router.delete('/songs/:_id', (req, res, next) => {
+//         Song.findByIdAndRemove(req.params._id)
+//             .then(() => {
+//                 res.send({ message: 'So much for that song' })
+//             })
+//             .catch(err => {
+//                 res.status(400).send({ Error: err })
+//             })
+//     }),
+//     router.put('/songs/:_id', (req, res, next) => {
+//         Song.findByIdAndUpdate(req.params._id, req.body)
+//             .then(() => {
+//                 res.send({ message: 'Updated Position' })
+//             })
+//             .catch(err => {
+//                 res.status(400).send({ Error: err })
+//             })
+//     }),
+// router.get('/songs', (req, res, next) => {
+//     Songs.find({})
+//         .then(songs => {
+//             res.send(songs)
+//         })
+//         .catch(err => {
+//             res.status(400).send({ Error: err })
+//         })
+// })
