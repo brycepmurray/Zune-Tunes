f6ed1b0d36c509d39cebd99b5a2174394a958d01
Bryce Murray <brycepmurray711@gmail.com>
2018-03-06 20:40:17 -0700

    changed Song model to fit new style for server and higher quality art

--- server/server-assets/models/song.js
+++ server/server-assets/models/song.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
-
+var Schema = mongoose.Schema
+var ObjectId = mongoose.SchemaTypes.ObjectId
+var schemaName = "Song"
 
 var schema = newmongoose.Schema({
     title: { type: String, required: true },
-    albumnArt: { type: String, required: true },
+    artworkUrl100: { type: String, required: true },
     artist: { type: String },
     albumn: { type: String },
     albumnPice: { type: Number, required: true },
     upvotes: { type: Number, required: true, default: 0 }
 });
 
-module.exports = mongoose.model('Song', schema)
+module.exports = mongoose.model(schemaName, schema)
