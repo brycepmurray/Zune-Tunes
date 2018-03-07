5e32767c33f5066be01939391c02c0621b4f979e
Bryce Murray <brycepmurray711@gmail.com>
2018-03-06 21:04:44 -0700

    commented out stretch goals

--- server/server-assets/routes/playlist-routes.js
+++ server/server-assets/routes/playlist-routes.js
@@ -2,34 +2,34 @@ var Songs = require('../models/song')
 var Playlists = require('../models/playlist')
 var router = require('express').Router()
 
-router.post('/mytunes', (req, res, next) => {
-        Playlist.create(req.body)
-            .then(playlist => {
-                res.send(playlist)
-            })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    }),
+// save for later when have more user functionality
 
-    router.delete('/mytunes/:id', (req, res, next) => {
-        Playlist.findByIdAndRemove(req.params.id)
-            .then(() => {
-                res.send({ message: 'playlist sucesfully deleted' })
-            })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    }),
+// router.post('/mytunes', (req, res, next) => {
+//         Playlist.create(req.body)
+//             .then(playlist => {
+//                 res.send(playlist)
+//             })
+//             .catch(err => {
+//                 res.status(400).send({ Error: err })
+//             })
+//     }),
 
-    router.get('/mytunes', (req, res, next) => {
-        Playlist.find({})
-            .then(playlist => {
-                res.send(playlist)
-            })
-            .catch(err => {
-                res.status(400).send({ Error: err })
-            })
-    })
+//     router.delete('/mytunes/:id', (req, res, next) => {
+//         Playlist.findByIdAndRemove(req.params.id)
+//             .then(() => {
+//                 res.send({ message: 'playlist sucesfully deleted' })
+//             })
+//             .catch(err => {
+//                 res.status(400).send({ Error: err })
+//             })
+//     }),
 
-module.exports = router
+router.get('/mytunes/playlist', (req, res, next) => {
+    Playlist.find()
+        .then(playlist => {
+            res.send(playlist)
+        })
+        .catch(next)
+})
+
+module.exports = { router }
